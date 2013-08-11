5835b9663681964aa8aea55f755f79e0de5c0ba5
atubo <err722@yahoo.com>
2013-08-11 11:45:44 -0700

    Added some helper functions.

--- LinkedList.h
+++ LinkedList.h
@@ -29,4 +29,25 @@ inline void printList(ListNode *head)
     }
 }
 
+inline int getListSize(ListNode *head) {
+    int count = 0;
+    ListNode *p = head;
+    while (p) {
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
+inline ListNode* getNthNode(ListNode *head, int n) {
+    int k = 1;
+    ListNode *p = head;
+    while (k != n) {
+        assert(p);
+        p = p->next;
+        k++;
+    }
+    return p;
+}
+
 #endif
