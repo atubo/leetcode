301edbbc3db8f829fcc39cb68788141035287487
atubo <err722@yahoo.com>
2013-08-18 22:59:42 -0700

    Added code to get vector from stdin.

--- Util.h
+++ Util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 
 #include <iostream>
+#include <vector>
 
 inline void printVector(const std::vector<int>& v)
 {
@@ -9,6 +10,18 @@ inline void printVector(const std::vector<int>& v)
         std::cout << v[i] << " ";
     }
     std::cout << std::endl;
+}
+
+inline std::vector<int> getVector()
+{
+    using std::cin;
+    int n;
+    cin >> n;
+    vector<int> result(n);
+    for (int i = 0; i < n; i++) {
+        cin >> result[i];
+    }
+    return result;
 }
 
 #endif
