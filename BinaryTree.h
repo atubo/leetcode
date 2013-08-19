4287fdece2688174c90edd68d5e30f1ce9733511
atubo <err722@yahoo.com>
2013-08-19 00:08:34 -0700

    Added code to construct binary tree from preorder and inorder data.

--- BinaryTree.h
+++ BinaryTree.h
@@ -2,6 +2,8 @@
 #define BINARYTREE_H
 
 #include <iostream>
+#include <vector>
+#include <cassert>
 
 struct TreeNode {
     int val;
@@ -23,5 +25,33 @@ inline void printTree(TreeNode *node)
     printTree(node->left);
     printTree(node->right);
 }
+
+inline TreeNode* createSubtree(std::vector<int> &preorder,
+                               std::vector<int>& inorder,
+                        int m1, int n1, int m2, int n2) {
+    assert(n1 - m1 == n2 - m2);
+    if (m1 == n1) return NULL;
+    int rval = preorder[m1];
+    int inorderPos = m2;
+    for (; inorderPos < n2; inorderPos++) {
+        if (inorder[inorderPos] == rval) break;
+    }
+    TreeNode *node = new TreeNode(rval);
+    int l1 = inorderPos - m2;
+    node->left = createSubtree(preorder, inorder,
+                               m1+1, m1+1+l1, m2, m2+l1);
+    node->right = createSubtree(preorder, inorder,
+                                m1+1+l1, n1, m2+l1+1, n2);
+    return node;
+}
+
+inline TreeNode *buildTree(std::vector<int> &preorder,
+                           std::vector<int>& inorder) {
+    assert(preorder.size() == inorder.size());
+    int n = preorder.size();
+    TreeNode *root = createSubtree(preorder, inorder, 0, n, 0, n);
+    return root;
+}
+
 
 #endif
