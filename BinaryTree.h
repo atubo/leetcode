e6d7ee8a250b05337a636ff1bb92306c311b8882
atubo <err722@yahoo.com>
2013-08-21 21:47:05 -0700

    Added code for linked tree.

--- BinaryTree.h
+++ BinaryTree.h
@@ -53,5 +53,58 @@ inline TreeNode *buildTree(std::vector<int> &preorder,
     return root;
 }
 
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x): val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+
+inline void printLinkedTree(TreeLinkNode *node)
+{
+    using std::cout;
+    using std::endl;
+
+    if (!node) {
+        cout << "# ";
+        return;
+    }
+    cout << node->val;
+    if (node->next) {
+        cout << "(" << node->next->val << ") ";
+    }
+    else {
+        cout <<"(nil) ";
+    }
+    printLinkedTree(node->left);
+    printLinkedTree(node->right);
+}
+
+inline TreeLinkNode* createSublinkedtree(std::vector<int> &preorder,
+                                         std::vector<int>& inorder,
+                                         int m1, int n1, int m2, int n2) {
+    assert(n1 - m1 == n2 - m2);
+    if (m1 == n1) return NULL;
+    int rval = preorder[m1];
+    int inorderPos = m2;
+    for (; inorderPos < n2; inorderPos++) {
+        if (inorder[inorderPos] == rval) break;
+    }
+    TreeLinkNode *node = new TreeLinkNode(rval);
+    int l1 = inorderPos - m2;
+    node->left = createSublinkedtree(preorder, inorder,
+                                     m1+1, m1+1+l1, m2, m2+l1);
+    node->right = createSublinkedtree(preorder, inorder,
+                                      m1+1+l1, n1, m2+l1+1, n2);
+    return node;
+}
+
+inline TreeLinkNode *buildLinkedTree(std::vector<int> &preorder,
+                                     std::vector<int>& inorder) {
+    assert(preorder.size() == inorder.size());
+    int n = preorder.size();
+    TreeLinkNode *root = createSublinkedtree(preorder, inorder, 0, n, 0, n);
+    return root;
+}
 
 #endif
