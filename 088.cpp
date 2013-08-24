a68d22d6f778bbb03e7688725b45fc702b1b1638
atubo <err722@yahoo.com>
2013-08-23 22:37:21 -0700

    Code for problem 88.

---
+++ 088.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 || j >= 0) {
+            if (i < 0) {
+                A[k--] = B[j--];
+            }
+            else if (j < 0) {
+                A[k--] = A[i--];
+            }
+            else {
+                if (A[i] > B[j]) {
+                    A[k--] = A[i--];
+                }
+                else {
+                    A[k--] = B[j--];
+                }
+            }
+        }
+    }
+};
+
+#if 0
+int main()
+{
+    int m;
+    int n;
+    cin >> m >> n;
+    int A[m+n];
+    for (int i = 0; i < m; i++) {
+        cin >> A[i];
+    }
+    int B[n];
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+    Solution s;
+    s.merge(A, m, B, n);
+    for (int i = 0; i < m + n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+};
+#endif
+
+
