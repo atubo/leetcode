9fdc762648f8962daf89cd1f68fd0fd2540a12aa
atubo <err722@yahoo.com>
2013-08-31 16:40:21 -0700

    Code for problem 84.

--- 084.cpp
+++ 084.cpp
@@ -4,41 +4,26 @@
 #include <iostream>
 using namespace std;
 
-struct Bar {
-    int x;
-    int y;
-    Bar(int x_, int y_): x(x_), y(y_) {}
-};
-
 class Solution {
 public:
     int largestRectangleArea(vector<int> &height) {
-        int n = height.size();
-        if (n == 0) return 0;
-        stack<Bar> barStack;
-        barStack.push(Bar(0, height[0]));
+        if (height.size() == 0) return 0;
+        vector<int> h = height;
+        h.push_back(0);
+        stack<int> barStack;
+        barStack.push(0);
         int maxArea = 0;
-        for (int i = 1; i < n; i++) {
-            if (height[i] < height[i-1]) {
-                while (!barStack.empty() && barStack.top().y > height[i]) {
-                    Bar bar = barStack.top();
+        for (int i = 1; i < (int)h.size(); i++) {
+            if (h[i] < h[i-1]) {
+                while (!barStack.empty() && h[barStack.top()] > h[i]) {
+                    int pos = barStack.top();
                     barStack.pop();
-                    int left = (barStack.empty() ? -1 : barStack.top().x);
-                    int area = bar.y * (i - left - 1);
+                    int left = (barStack.empty() ? -1 : barStack.top());
+                    int area = h[pos] * (i - left - 1);
                     maxArea = max(area, maxArea);
                 }
-                barStack.push(Bar(i, height[i]));
             }
-            if (height[i] >= height[i-1]) {
-                barStack.push(Bar(i, height[i]));
-            }
-        }
-        while (!barStack.empty()) {
-            Bar bar = barStack.top();
-            barStack.pop();
-            int left = (barStack.empty() ? -1 : barStack.top().x);
-            int area = bar.y * (n - left - 1);
-            maxArea = max(maxArea, area);
+            barStack.push(i);
         }
         return maxArea;
     }
