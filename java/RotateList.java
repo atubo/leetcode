59579f1a24bb923c145079d3e2605f238622fd71
atubo <err722@yahoo.com>
2014-05-11 14:58:20 -0700

    Initial version

---
+++ java/RotateList.java
@@ -0,0 +1,36 @@
+import LeetcodeUtil.*;
+import java.util.*;
+
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return head;
+        int n = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            n++;
+            tail = tail.next;
+        }
+        
+        if (k == 0 || k == n) return head;
+        
+        ListNode curr = head;
+        for (int i = 1; i < n-k; i++) {
+            curr = curr.next;
+        }
+        ListNode tmp = curr.next;
+        curr.next = null;
+        tail.next = head;
+        head = tmp;
+        
+        return head;
+    }
+    
+    public static void main(String[] args) {
+        InputReader reader = new InputReader();
+        int k = reader.readInt();
+        ListNode head = reader.readList();
+        RotateList solution = new RotateList();
+        head = solution.rotateRight(head, k);
+        ListNode.printList(head);
+    }
+}
